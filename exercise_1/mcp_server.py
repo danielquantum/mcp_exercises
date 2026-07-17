274e9d099b56630718f7fcabe279b1eb730bde7f
danielquantum <danielquantum2008@gmail.com>
2026-07-17 11:37:38 +0700

    (update) MCP Server: Edit document implementation

--- exercise_1/mcp_server.py
+++ exercise_1/mcp_server.py
@@ -26,6 +26,20 @@ def read_document(
     return docs[doc_id]
 
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_doc",
+    description="Edit a document by replacing a string in the documents content with a new string.",
+)
+def edit_document(
+    doc_id: str = Field(description="Id of the document that will be edited."),
+    old_str: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text."),
+):
+    if doc_id not in docs:
+        return ValueError(f"Document with id '{doc_id}' not found.")
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+    return docs[doc_id]
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
